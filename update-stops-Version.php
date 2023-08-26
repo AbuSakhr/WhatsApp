2fa4fa9699ba08a3cfc2853d009955c18c4dacda
AbuSakhr <119666007+AbuSakhr@users.noreply.github.com>
2023-08-26 19:51:33 +0300

    Update update-stops-Version.php

--- update-stops-Version.php
+++ update-stops-Version.php
@@ -1,1 +1,1 @@
-15||https://whatsaber2.blogspot.com/2022/10/WhatSaber.html/||https://whatsaber2.blogspot.com/2022/10/WhatSaber.html/
+25||https://whatsaber2.blogspot.com/2022/10/WhatSaber.html/||https://whatsaber2.blogspot.com/2022/10/WhatSaber.html/
